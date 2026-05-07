6ef45c6998265e94b3ddf311227ccb7fea6fe270
aaree <supercat159@gmail.com>
2026-05-07 10:06:02 -0400

    Add: longest-palindromic-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        memo={}
+        def dfs(left,right):
+            if right<left:
+                return 0
+            if right==left:
+                if s[right]==s[left]:
+                    return 1
+                else:
+                    return 0
+            if (left,right) in memo:
+                return memo[(left,right)]
+            temp1=temp2=temp3=0
+            if s[left]==s[right]:
+                temp1=2+dfs(left+1,right-1)
+            if s[left]!=s[right]:
+                temp2=dfs(left+1,right)
+                temp3=dfs(left,right-1)
+            memo[(left,right)]= max(temp1,temp2,temp3)
+            return memo[(left,right)]
+        return dfs(0,len(s)-1)
