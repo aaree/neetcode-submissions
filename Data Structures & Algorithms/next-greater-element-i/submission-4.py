04dc843ee06f86036c030df929876f0fbdbba7f8
aaree <supercat159@gmail.com>
2026-05-09 19:16:34 -0400

    Add: next-greater-element-i - submission-4

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans=[-1]*len(nums1)
+        temp={}
+        stack=[]
+        
+        for i, num in enumerate(nums2):
+            if len(stack)==0:
+                stack.append((i,num))
+            else:
+                if stack[-1][1]>num:
+                    stack.append((i,num))
+                else:
+                    while stack and num>stack[-1][1]:
+                        i2,num2=stack.pop()
+                        temp[num2]=num
+                    stack.append((i,num))
+        for i, num in enumerate(nums1):
+            if num in temp:
+                ans[i]=temp[num]
+        return ans
+        
