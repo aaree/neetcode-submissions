0ea3b779f903cabf9003711fa18499b8274cc5ff
aaree <supercat159@gmail.com>
2026-05-09 14:20:33 -0400

    Add: two-city-scheduling - submission-2

---
+++ Data Structures & Algorithms/two-city-scheduling/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        memo={}
+        def dfs(a,b,pointer):
+            if pointer==len(costs):
+                return 0
+            if (a,b) in memo:
+                return memo[(a,b)]
+            a1=math.inf
+            b1=math.inf
+            if a>0:
+                a1=costs[pointer][0]+dfs(a-1,b,pointer+1)
+            if b>0:
+                b1=costs[pointer][1]+dfs(a,b-1,pointer+1)
+            memo[(a,b)]= min(a1,b1)
+            return memo[(a,b)]
+        return dfs(len(costs)/2,len(costs)/2,0)                
