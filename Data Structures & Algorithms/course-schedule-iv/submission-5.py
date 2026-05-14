89f5487b854fddc94f28d011c79b0c0b5a54772d
aaree <supercat159@gmail.com>
2026-05-14 18:33:29 -0400

    Add: course-schedule-iv - submission-5

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        seen=set()
+        ans=[]
+        adj={}
+        for i in range(numCourses):
+            adj[i]=[]
+        for a,b in prerequisites:
+            adj[a].append(b)
+
+        def dfs(course,target):
+            if course==target:
+                return True
+            if course in seen:
+                return False
+            if course in adj:
+                for c in adj[course]:
+                    if dfs(c,target):
+                        return True
+            seen.add(course)
+            ans.append(course)
+        final=[]
+        for a,b in queries:
+            res=dfs(a,b)
+            if res is None:
+                res=False
+            final.append(res)
+            seen=set()
+        return final
+        
