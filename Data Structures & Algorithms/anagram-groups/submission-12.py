d90280c68ab7349804885356d0e19087a9726480
aaree <supercat159@gmail.com>
2026-08-17 00:27:29 -0400

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        temp={}
+        for st in strs:
+            arr=[0]*26
+            for i in st:
+                num=ord(i)-ord('a')
+                arr[num]+=1
+            arr=tuple(arr)
+            if arr in temp:
+                temp[arr].append(st)
+            else:
+                temp[arr]=[]
+                temp[arr].append(st)
+        return list(temp.values())
